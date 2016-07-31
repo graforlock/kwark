928ef2330fb3f44e5f548295ff44d31042c8134e
Maciej Sitko <maciej@neatcube.co.uk>
2016-07-31 22:59:34 +0100

    Ajax

--- kwark-async-ajax.js
+++ kwark-async-ajax.js
@@ -1,10 +1,27 @@
 kwark = kwark || {};
 
-function ajax(options) {}
+function ajax(options) {
+    var xhttp = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
+    xhttp.onreadystatechange = function() {
+        if (xhttp.readyState == 4 && xhttp.status == 200) return xhttp.responseText;
+    };
+    if(options.method === 'get') {
+        xhttp.open("GET", options.url, true);
+        xhttp.send();
+    }
+    if(options.method === 'post') {
+        xhttp.open("POST", options.url, true);
+        xhttp.send();
+    }
+}
 
-function get() {}
+function get(options) {
+    return kwark.promise({method: 'get'})
+}
 
-function post() {}
+function post(options) {
+    return kwark.promise({method: 'post'})
+}
 
 kwark.ajax = ajax;
 kwark.post = post;
