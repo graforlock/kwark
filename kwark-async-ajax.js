f504be096c21de5485d9657b44d2fea2e6c8538a
Maciej Sitko <maciej@neatcube.co.uk>
2016-08-01 18:29:38 +0100

    Status added for safety

--- kwark-async-ajax.js
+++ kwark-async-ajax.js
@@ -1,36 +1,43 @@
 kwark = kwark || {};
 
 function ajax(url) {
-    var state = 0,
-        deferred,
-        value;
-    
-    function resolve(result) {
-        value = result;
-        state = result.readyState;
+    if(this instanceof ajax) {
+        var state = 0,
+            status = 0,
+            deferred,
+            value;
+        
+        function resolve(result) {
+            value = result;
+            state = result.readyState;
+            status = result.status;
 
-        if(deferred) {
-            handle(deferred);
+            if(deferred) {
+                handle(deferred);
+            }
         }
-    }
 
-    function handle(onResolved) {
-        if( state !== 4) {
-            deferred = onResolved;
+        function handle(onResolved) {
+            if( state !== 4 && status !== 200) {
+                deferred = onResolved;
+            }
+            onResolved(value.responseText)
         }
-        onResolved(value.responseText)
-    }
-    
-    this.then = function(resolved) {
-        handle(resolved);
-    }
+        
+        this.then = function(resolved) {
+            handle(resolved);
+        }
 
-    var xhttp = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
-    xhttp.onreadystatechange = function() {
-        resolve(xhttp);
-    };
-    xhttp.open("GET", url, true);
-    xhttp.send();
+        var xhttp = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
+        xhttp.onreadystatechange = function() {
+            resolve(xhttp);
+        };
+        xhttp.open("GET", url, true);
+        xhttp.send();
+
+    } else {
+        return new ajax(url);
+    }
 
 }
 
