7ed24e029af7f138fdb828b21606338530a9e83b
Maciej Sitko <maciej@neatcube.co.uk>
2016-08-08 21:53:44 +0200

    Compose refined

--- tests/compose-ajax.test.js
+++ tests/compose-ajax.test.js
@@ -3,18 +3,24 @@ var test = require('tape'),
     compose = require('../src/kwark-core.js').compose;
 
 test('some test', function(t) {
-    t.plan(1);
+    t.plan(2);
 
     function toJSON(response) {
         return JSON.parse(response);
     }
 
-    function assert(response) {
+    function assertObject(response) {
         t.equal(typeof response, 'object');
+        return response;
+    }
+
+    function assertContent(response) {
+        t.equal(typeof response.id, 'number');
         t.end();
     }
 
-    var composed = compose(assert, toJSON);
+    var testSuites = compose(assertContent, assertObject);
+    var composed = compose(testSuites, toJSON);
 
     result = ajax('http://jsonplaceholder.typicode.com/posts/1')
         .then(composed);
