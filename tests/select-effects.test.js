8edc8090d38a3fac4e8742f164af60a3ac5e193b
Maciej Sitko <maciej@neatcube.co.uk>
2016-08-08 21:45:30 +0200

    Spaces for readability

--- tests/select-effects.test.js
+++ tests/select-effects.test.js
@@ -4,13 +4,17 @@ var select = require('../src/kwark-dom-select'),
 
 test('nodeify element and click it', function(t) { 
     t.plan(2);
+
     var clicky = select.inline('<div id="sample-id"><p>Heyhoue</p></div>').nodeify().find('p');
     t.equal(typeof clicky.node, 'object');
+
     clicky.click(function(ev) {
         t.equal(clicky.type(ev), '[object MouseEvent]');
         t.end();
     });
+
     clicky.node.click();
+
 });
 
 
