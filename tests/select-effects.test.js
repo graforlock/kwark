432100bc5fdb868b4e550a34420e7776de04ccd9
Maciej Sitko <maciej@neatcube.co.uk>
2016-08-08 20:26:47 +0200

    Test added and some changes

---
+++ tests/select-effects.test.js
@@ -0,0 +1,15 @@
+var select = require('../src/kwark-dom-select'),
+    effects = require('../src/kwar-dom-select'),
+    test = require('tape');
+
+test('select and nodeify', function(t) { 
+    t.plan(1);
+    t.end();
+});
+
+test('fade out after three seconds', function(t) { 
+    t.plan(1);
+    t.end();
+});
+
+
