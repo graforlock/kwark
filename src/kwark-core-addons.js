40e822988127478057670f1391417f054ee5ba35
Maciej Sitko <maciej@neatcube.co.uk>
2016-10-23 16:59:37 +0100

    Formatted addons

--- src/kwark-core-addons.js
+++ src/kwark-core-addons.js
@@ -1,18 +1,25 @@
 var addons = {
     serialize: {
-        form: function(form) {
+        form: function (form)
+        {
             var field, s = [];
-            if (typeof form == 'object' && form.nodeName == "FORM") {
+            if (typeof form == 'object' && form.nodeName == "FORM")
+            {
                 var len = form.elements.length;
-                for (i=0; i<len; i++) {
+                for (i = 0; i < len; i++)
+                {
                     field = form.elements[i];
-                    if (field.name && !field.disabled && field.type != 'file' && field.type != 'reset' && field.type != 'submit' && field.type != 'button') {
-                        if (field.type == 'select-multiple') {
-                            for (j=form.elements[i].options.length-1; j>=0; j--) {
-                                if(field.options[j].selected)
+                    if (field.name && !field.disabled && field.type != 'file' && field.type != 'reset' && field.type != 'submit' && field.type != 'button')
+                    {
+                        if (field.type == 'select-multiple')
+                        {
+                            for (j = form.elements[i].options.length - 1; j >= 0; j--)
+                            {
+                                if (field.options[j].selected)
                                     s[s.length] = encodeURIComponent(field.name) + "=" + encodeURIComponent(field.options[j].value);
                             }
-                        } else if ((field.type != 'checkbox' && field.type != 'radio') || field.checked) {
+                        } else if ((field.type != 'checkbox' && field.type != 'radio') || field.checked)
+                        {
                             s[s.length] = encodeURIComponent(field.name) + "=" + encodeURIComponent(field.value);
                         }
                     }
@@ -20,41 +27,53 @@ var addons = {
             }
             return s.join('&').replace(/%20/g, '+');
         },
-        object: function(obj) {
+        object: function (obj)
+        {
             var str = [];
-            for(var p in obj)
-                if (obj.hasOwnProperty(p)) {
+            for (var p in obj)
+                if (obj.hasOwnProperty(p))
+                {
                     str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
                 }
             return str.join("&");
         }
     },
-    xmlToJson: function(xml){
+    xmlToJson: function (xml)
+    {
         // Create the return object
         var obj = {};
 
-        if (xml.nodeType == 1) { // element
+        if (xml.nodeType == 1)
+        { // element
             // do attributes
-            if (xml.attributes.length > 0) {
-            obj["@attributes"] = {};
-                for (var j = 0; j < xml.attributes.length; j++) {
+            if (xml.attributes.length > 0)
+            {
+                obj["@attributes"] = {};
+                for (var j = 0; j < xml.attributes.length; j++)
+                {
                     var attribute = xml.attributes.item(j);
                     obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
                 }
             }
-        } else if (xml.nodeType == 3) { // text
+        } else if (xml.nodeType == 3)
+        { // text
             obj = xml.nodeValue;
         }
 
         // do children
-        if (xml.hasChildNodes()) {
-            for(var i = 0; i < xml.childNodes.length; i++) {
+        if (xml.hasChildNodes())
+        {
+            for (var i = 0; i < xml.childNodes.length; i++)
+            {
                 var item = xml.childNodes.item(i);
                 var nodeName = item.nodeName;
-                if (typeof(obj[nodeName]) == "undefined") {
+                if (typeof(obj[nodeName]) == "undefined")
+                {
                     obj[nodeName] = xmlToJson(item);
-                } else {
-                    if (typeof(obj[nodeName].push) == "undefined") {
+                } else
+                {
+                    if (typeof(obj[nodeName].push) == "undefined")
+                    {
                         var old = obj[nodeName];
                         obj[nodeName] = [];
                         obj[nodeName].push(old);
@@ -65,18 +84,24 @@ var addons = {
         }
         return obj;
     },
-    loadScript: function(source, callback) {
+    loadScript: function (source, callback)
+    {
         var script = document.createElement('script');
         var prior = document.getElementsByTagName('script')[0];
         script.async = 1;
         prior.parentNode.insertBefore(script, prior);
 
-        script.onload = script.onreadystatechange = function( _, isAbort ) {
-            if(isAbort || !script.readyState || /loaded|complete/.test(script.readyState) ) {
+        script.onload = script.onreadystatechange = function (_, isAbort)
+        {
+            if (isAbort || !script.readyState || /loaded|complete/.test(script.readyState))
+            {
                 script.onload = script.onreadystatechange = null;
                 script = undefined;
 
-                if(!isAbort) { if(callback) callback(); }
+                if (!isAbort)
+                {
+                    if (callback) callback();
+                }
             }
         };
 
